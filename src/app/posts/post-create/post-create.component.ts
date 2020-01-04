1d7207520a72befef684d47a654bf238ffac6364
Gerard Munoz <gmunoz@pop-os.localdomain>
2020-01-04 11:28:54 -0500

    added injectable service

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -6,9 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'No Content';
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  enteredContent = '';
+  enteredTitle = '';
+
+  constructor(public postsService: PostsService) { }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) { return; }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
